c86095392637e93bb36b1c41876a3d59bb98f952
Максим Дзюбенко <maksim.dzjubenko@ivkhk.ee>
2022-05-21 19:22:02 +0300

    added the alert, when click on the cart.

--- js/index.js
+++ js/index.js
@@ -14,3 +14,21 @@ for(let i = 0; i < card.length; i++) {
         cartContainer[i].classList.remove("active");
     }
 }
+const btnToCart = document.getElementsByClassName("inShoppingCart");
+for(let i = 0; i < card.length; i++) {
+    btnToCart[i].addEventListener('click', addToCart);
+    function addToCart() {
+        const message = document.getElementById("message");
+        message.classList.add("active");
+        setTimeout(() => message.classList.remove("active"), 2000);
+    }
+}
+window.addEventListener('scroll', function () {
+    var pixelCount = window.pageYOffset;
+    if (pixelCount < 480) {
+        message.style.top = "20px"
+    }
+    if (pixelCount > 480) {
+        message.style.top = "80px"
+    }
+})
