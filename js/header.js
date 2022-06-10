c30935eac2bab8636b6617717ea049b3dfad14fc
Максим Дзюбенко <maksim.dzjubenko@ivkhk.ee>
2022-06-10 21:56:36 +0300

    Сделал мобилную навигацию до конца(для проверки сжимайте экран, т.к. пока что ивентЛисенер работает как resize).

--- js/header.js
+++ js/header.js
@@ -1,11 +1,6 @@
-var debug = true;
-function isDebug(message) {
-    if(debug) console.log(message);
-}
-
 // BURGER MENU
-const burger = document.getElementById('burgerContainer');
-const burgerNav = document.getElementById('topnav');
+burger = document.getElementById('burgerContainer');
+burgerNav = document.getElementById('topnav');
 burger.addEventListener('click', (e) => {
     e.preventDefault();
     burger.classList.toggle('change');
@@ -16,20 +11,18 @@ burger.addEventListener('click', (e) => {
         setTimeout(() => {
             burgerNav.style.opacity = '1';
             burgerNav.style.transform = 'translateY(0%)';
-            document.getElementById('categories').style.marginTop = '210px';
         }, 10);
     }
     else {
         // HIDE BURGER MENU
         burgerNav.style.opacity = '0';
         burgerNav.style.transform = 'translateY(-70%)';
-        document.getElementById('categories').style.marginTop = '0';
         setTimeout(() => {
             burgerNav.style.display = 'none';
         }, 400);
     }
 });
-// HEADER TYPE
+// HEADER TYPE (resize page)
 window.addEventListener('resize', (e) => {
     var widthPixel = window.innerWidth;
     // SHOW MOBILE HEADER
@@ -54,7 +47,6 @@ window.addEventListener('resize', (e) => {
         }
         burgerNav.style.opacity = '0';
         burgerNav.style.transform = 'translateY(-70%)';
-        document.getElementById('categories').style.marginTop = '0';
         setTimeout(() => {
             burgerNav.style.display = 'none';
         }, 400);
@@ -85,50 +77,3 @@ window.addEventListener('resize', (e) => {
         }
     }
 });
-
-// FURNITURE HOVER
-const card = document.getElementsByClassName("particular-object");
-const cartContainer = document.getElementsByClassName("shopping-cart-container");
-for(let i = 0; i < card.length; i++) {
-    card[i].addEventListener('mousemove', showShoppingCart);
-    card[i].addEventListener('mouseout', hideShoppingCart);
-    function showShoppingCart() {
-        cartContainer[i].style.zIndex = "99999;"
-        card[i].classList.add("active");
-        cartContainer[i].classList.add("active");
-    };
-    function hideShoppingCart() {
-        cartContainer[i].style.zIndex = "100;"
-        card[i].classList.remove("active");
-        cartContainer[i].classList.remove("active");
-    };
-}
-
-// SHOPPING CART ALERT
-const btnToCart = document.getElementsByClassName("inShoppingCart");
-for(let i = 0; i < card.length; i++) {
-    btnToCart[i].addEventListener('click', addToCart);
-    function addToCart() {
-        const message = document.getElementById("message");
-        message.classList.add("active");
-        setTimeout(() => message.classList.remove("active"), 3000);
-    }
-}
-
-// TYPE HEADING SHADOW
-const furnitureType = document.getElementsByTagName('h2');
-window.addEventListener('scroll', function () {
-    var pixelCount = window.pageYOffset;
-    if (pixelCount < 540) {
-        message.style.top = "20px";
-        for(let i = 0; i < card.length; i++) {
-            furnitureType[i].style.boxShadow = "0 0px 0px 0 rgba(0,0,0,0.2)";
-        }
-    }
-    if (pixelCount > 540) {
-        message.style.top = "80px";
-        for(let i = 0; i < card.length; i++) {
-            furnitureType[i].style.boxShadow = "0 8px 8px 0 rgba(0,0,0,0.2)";
-        }
-    }
-})
