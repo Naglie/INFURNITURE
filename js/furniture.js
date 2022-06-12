1689792a47168fe90be31a9875ab8dd1ac205974
Максим Дзюбенко <maksim.dzjubenko@ivkhk.ee>
2022-06-12 19:00:44 +0300

    Убрал модульный скрипт, все работает как швейцарские часы.

--- js/furniture.js
+++ js/furniture.js
@@ -1,4 +1,13 @@
-import {comboBoxModule} from './ComboBoxModule.js';
+// import {comboBoxModule} from './ComboBoxModule.js';
+var xhr= new XMLHttpRequest();
+xhr.open('GET', 'productInfo.html', true);
+xhr.onreadystatechange= function() {
+    if (this.readyState !== 4) return;
+    if (this.status !== 200) return;
+    document.getElementById('hidden-info').innerHTML = this.responseText;
+};
+xhr.send();
+
 
 // BURGER MENU
 const burger = document.getElementById('burgerContainer');
@@ -101,37 +110,6 @@ for(let i = 0; i < card.length; i++) {
     };
 }
 
-// COMBOBOX
-const partObject = document.getElementsByClassName('particular-object');
-for (let i = 0; i < card.length; i++) {
-    if(card[i].childElementCount === 5) {
-        var productImgClick = card[i].children[1]
-    }
-    if(card[i].childElementCount === 4) {
-        var productImgClick = card[i].children[0]
-    }
-    productImgClick.addEventListener('click', (e) => {
-        const comboBox = document.getElementById('comboBox-container').style.display = 'unset';
-        let productChildren = partObject[i].children;
-        if(partObject[i].childElementCount === 5) {
-            var productImg = productChildren[1].getAttribute('src');
-            var productName = productChildren[2].textContent;
-            var productNewPrice = productChildren[3].children[0].textContent;
-            var productOldPrice = productChildren[3].children[1].textContent;
-        }
-        else if(partObject[i].childElementCount === 4) {
-            var productImg = productChildren[0].getAttribute('src');
-            var productName = productChildren[1].textContent;
-            var productNewPrice = productChildren[2].textContent;
-        }
-        comboBoxModule.createComboBox(productImg, productName, productNewPrice, productOldPrice);
-        document.getElementById('dark-screen').style.display = 'unset';
-        document.getElementById('dark-screen').style.opacity = '1';
-        document.getElementById('message').style.display = 'none';
-        // console.log(productImg, productName, productNewPrice, productOldPrice)
-    })
-}
-
 // SHOPPING CART ALERT
 const btnToCart = document.getElementsByClassName('inShoppingCart');
 for(let i = 0; i < card.length; i++) {
@@ -164,5 +142,138 @@ function setShadow() {
     document.getElementById('message').style.top = "80px";
     for(let i = 0; i < furnitureType.length; i++) {
         furnitureType[i].style.boxShadow = '0 8px 8px 0 rgba(0,0,0,0.2)';
+    }
+}
+
+
+// COMBOBOX
+const partObject = document.getElementsByClassName('particular-object');
+for (let i = 0; i < card.length; i++) {
+    if(card[i].childElementCount === 5) {
+        var productImgClick = card[i].children[1]
+    }
+    if(card[i].childElementCount === 4) {
+        var productImgClick = card[i].children[0]
+    }
+    productImgClick.addEventListener('click', (e) => {
+        const comboBox = document.getElementById('comboBox-container').style.display = 'unset';
+        let productChildren = partObject[i].children;
+        if(partObject[i].childElementCount === 5) {
+            var productImg = productChildren[1].getAttribute('src');
+            var productName = productChildren[2].textContent;
+            var productNewPrice = productChildren[3].children[0].textContent;
+            var productOldPrice = productChildren[3].children[1].textContent;
+        }
+        else if(partObject[i].childElementCount === 4) {
+            var productImg = productChildren[0].getAttribute('src');
+            var productName = productChildren[1].textContent;
+            var productNewPrice = productChildren[2].textContent;
+        }
+        createComboBox(productImg, productName, productNewPrice, productOldPrice);
+        document.getElementById('dark-screen').style.display = 'unset';
+        document.getElementById('dark-screen').style.opacity = '1';
+        document.getElementById('message').style.display = 'none';
+        // console.log(productImg, productName, productNewPrice, productOldPrice)
+    })
+}
+
+
+function createComboBox(productImg, productName, productNewPrice, productOldPrice) {
+    const comboBox = document.getElementById('comboBox-container');
+    comboBox.style.display = 'unset';
+    comboBox.innerHTML = 
+    `<div id="comboBox">
+        <div id="box-exit" class="box-exit-btn"><img src="../Images/Other_images/Group 21.png"></div>
+        <div class="box-img_info-container">
+            <div class="box-img-container">
+                <img id="product-img" src="" alt="product image">
+            </div>
+            <div class="box-info-container">
+                <p id="product-name"></p>
+                <div class="product-prices">
+                    <p id="product-new-price"></p>
+                    <p id="product-old-price"></p>
+                </div>
+                <div id="shpCartBtn" class="box-button">
+                    <a href="">В корзину</a>
+                </div>
+            </div>
+        </div>
+        <p id="box-characteristics-name">Характеристики</p>
+        <div class="box-characteristics-container">
+            <div class="box-characteristics1">
+                <p id="product-1"></p>
+                <p id="product-2"></p>
+                <p id="product-3"></p>
+                <p id="product-4"></p>
+            </div>
+            <div class="box-characteristics2">
+                <p id="product-5"></p>
+                <p id="product-6"></p>
+                <p id="product-7"></p>
+            </div>
+        </div>
+    </div`;
+
+    // FUNCTION TO HIDE THE COMBOBOX
+    function hideComboBox() {
+        comboBox.innerHTML = '';
+        comboBox.style.display = 'none';
+        document.getElementById('dark-screen').style.opacity = '0';
+        setTimeout(() => {
+            document.getElementById('dark-screen').style.display = 'none';
+            document.getElementById('message').style.display = 'flex';
+        }, 200);
+    }
+
+    // LISTENERS TO CLOSE THE COMBOBOX
+    document.getElementById('box-exit').addEventListener('click', (e) => {
+        e.preventDefault();
+        hideComboBox()
+    });
+    document.getElementById('shpCartBtn').addEventListener('click', (e) => {
+        e.preventDefault();
+        hideComboBox()
+    });
+    document.getElementById('dark-screen').addEventListener('click', (e) => {
+        e.preventDefault();
+        hideComboBox()
+    });
+    
+    if(productOldPrice !== undefined) {
+        document.getElementById('product-old-price').style.display = 'unset';
+        document.getElementById('product-new-price').style.color = '#E63333';
+        document.getElementById('product-img').setAttribute('src', productImg);
+        document.getElementById('product-name').innerHTML = productName;
+        document.getElementById('product-new-price').innerHTML = productNewPrice;
+        document.getElementById('product-old-price').innerHTML = productOldPrice;
+    }
+    if(productOldPrice === undefined) {
+        document.getElementById('product-img').setAttribute('src', productImg);
+        document.getElementById('product-name').innerHTML = productName;
+        document.getElementById('product-new-price').innerHTML = productNewPrice;
+        document.getElementById('product-old-price').style.display = 'none';
+        document.getElementById('product-new-price').style.color = 'black';
+    }
+    const infoBlock = document.getElementsByClassName('product-info-block');
+    for (let i = 0; i < infoBlock.length; i++) {
+        let blockChildren = infoBlock[i].children;
+        if(blockChildren[0].textContent === productName) {
+            var characteristic1 = blockChildren[1].textContent;
+            var characteristic2 = blockChildren[2].textContent;
+            var characteristic3 = blockChildren[3].textContent;
+            var characteristic4 = blockChildren[4].textContent;
+            var characteristic5 = blockChildren[5].textContent;
+            var characteristic6 = blockChildren[6].textContent;
+            var characteristic7 = blockChildren[7].textContent;
+
+            document.getElementById('product-1').innerHTML = characteristic1;
+            document.getElementById('product-2').innerHTML = characteristic2;
+            document.getElementById('product-3').innerHTML = characteristic3;
+            document.getElementById('product-4').innerHTML = characteristic4;
+            document.getElementById('product-5').innerHTML = characteristic5;
+            document.getElementById('product-6').innerHTML = characteristic6;
+            document.getElementById('product-7').innerHTML = characteristic7;
+        }
     }
 }
