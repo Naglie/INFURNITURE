672406b60e77ff6ec7ceb0a96547b494544aa247
Максим Дзюбенко <maksim.dzjubenko@ivkhk.ee>
2022-06-13 21:04:06 +0300

    Сделал полную адаптивность на странице sales.html, потно.

--- js/header.js
+++ js/header.js
@@ -1,31 +1,3 @@
-// BURGER MENU
-burger = document.getElementById('burgerContainer');
-burgerNav = document.getElementById('topnav');
-burger.addEventListener('click', (e) => {
-    e.preventDefault();
-    burger.classList.toggle('change');
-    burgerNav.classList.toggle('hide');
-    if(burgerNav.classList.contains('hide')) {
-        // SHOW BURGER MENU
-        burgerNav.style.display = 'unset';
-        setTimeout(() => {
-            burgerNav.style.opacity = '1';
-            burgerNav.style.transform = 'translateY(0%)';
-            document.getElementsByTagName('header')[0].style.marginBottom = '250px';
-        }, 10);
-    }
-    else {
-        // HIDE BURGER MENU
-        burgerNav.style.opacity = '0';
-        burgerNav.style.transform = 'translateY(-70%)';
-        document.getElementsByTagName('header')[0].style.marginBottom = '0';
-        setTimeout(() => {
-            burgerNav.style.display = 'none';
-        }, 400);
-    }
-});
-
-
 // HEADER TYPE (resize page, BUT better to use load)
 window.addEventListener('resize', (e) => {
     var widthPixel = window.innerWidth;
---
+++ js/salesBurger.js
@@ -0,0 +1,26 @@
+// BURGER MENU
+burger = document.getElementById('burgerContainer');
+burgerNav = document.getElementById('topnav');
+burger.addEventListener('click', (e) => {
+    e.preventDefault();
+    burger.classList.toggle('change');
+    burgerNav.classList.toggle('hide');
+    if(burgerNav.classList.contains('hide')) {
+        // SHOW BURGER MENU
+        burgerNav.style.display = 'unset';
+        setTimeout(() => {
+            burgerNav.style.opacity = '1';
+            burgerNav.style.transform = 'translateY(0%)';
+            // document.getElementsByTagName('header')[0].style.marginBottom = '250px';
+        }, 10);
+    }
+    else {
+        // HIDE BURGER MENU
+        burgerNav.style.opacity = '0';
+        burgerNav.style.transform = 'translateY(-70%)';
+        // document.getElementsByTagName('header')[0].style.marginBottom = '0';
+        setTimeout(() => {
+            burgerNav.style.display = 'none';
+        }, 400);
+    }
+});
